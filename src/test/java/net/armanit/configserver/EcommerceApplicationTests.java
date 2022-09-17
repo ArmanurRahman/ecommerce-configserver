ba5c19093374b8b8a15877db66c02bd8068eecc4
Armanur Rahman <mubeenarman19@gmail.com>
2022-09-17 16:31:33 +0900

    initial commit config server

---
+++ src/test/java/net/armanit/configserver/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.armanit.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
